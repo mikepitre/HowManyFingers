722218c14a44b131e075e5ddecc020af2b79d5ec
mikepitre <michaelepitre@gmail.com>
2015-09-19 14:02:56 -0500

    guess functionality working with answer

--- HowManyFingers?/ViewController.swift
+++ HowManyFingers?/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var guessField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func submitGuess(sender: AnyObject) {
+        
+        let fingers = Int(arc4random_uniform(6))
+        
+        if Int(guessField.text!) == fingers {
+            resultLabel.text = "You guessed it! I was holding up \(fingers) fingers!"
+        } else {
+            resultLabel.text = "Sorry that's wrong. I was holding up \(fingers) fingers."
+        }
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
